1e516cfc5357da34f32e9aebd1d11b2a34db9d89
chocora <taiki.murakami.2v@stu.hosei.ac.jp>
2019-06-27 10:09:47 +0900

    中間発表時点

---
+++ map.py
@@ -0,0 +1,10 @@
+from japanmap import *
+
+
+
+#print(pref_names[1])
+%matplotlib inline
+
+import matplotlib.pyplot as plt
+plt.imshow(picture());
+
--- python_api.py
+++ python_api.py
@@ -1,13 +1,25 @@
 import requests
 import json
 
-city_name="Tokyo"
+#city_name="Tokyo"
 API_KEY = "0a5357d6b04726d418aad4cca2202999"
 api = "http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=ja&appid={key}"
 
-url = api.format(city = city_name, key = API_KEY)
-print(url)
-response = requests.get(url)
-data = response.json()
-jsonText = json.dumps(data, indent=4)
-print(jsonText)
+
+#print(url)
+cities=["Tokyo,JP","London,UK"]
+
+
+
+#jsonText = json.dumps(data, indent=4)#json.dumpでjson型に変換
+#print(data)
+#print(jsonText)
+
+
+for city_name in cities:
+    url = api.format(city = city_name, key = API_KEY)
+    response = requests.get(url)
+    data = json.loads(response.text)
+    print("+ 都市=",data["name"])
+    print("| 天気=", data["weather"])
+    print(" ")
